c470726ac3a68eb43e101463912a3b4ba9eb90ab
聪聪不是冲冲 <862348941@qq.com>
2025-05-21 13:56:08 +0800

    feat(deploy): deploy on github2.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,11 +12,6 @@ export default defineNuxtConfig({
     ssr: false,
     app: {
         baseURL: '/nuxt3-ssg-template/',
-        buildAssetsDir: 'nuxt_assets',
-    },
-    // 关闭_payload.json文件 为了在github上部署时不报错 github不支持_开头的文件
-    experimental: {
-        payloadExtraction: false
     },
     compatibilityDate: '2025-05-15',
     devtools: {enabled: true}
