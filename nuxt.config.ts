0f6c8b47493edba338da3d042708ab8611c27309
聪聪不是冲冲 <862348941@qq.com>
2025-05-21 13:43:11 +0800

    feat(deploy): deploy on github

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,14 @@ export default defineNuxtConfig({
     ],
     css: ['@/assets/css/tailwind.css'],
     ssr: false,
+    app: {
+        baseURL: '/nuxt3-ssg-template/',
+        buildAssetsDir: 'nuxt_assets',
+    },
+    // 关闭_payload.json文件 为了在github上部署时不报错 github不支持_开头的文件
+    experimental: {
+        payloadExtraction: false
+    },
     compatibilityDate: '2025-05-15',
     devtools: {enabled: true}
 })
