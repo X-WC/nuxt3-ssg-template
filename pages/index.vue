ade92080f2bf7cb43b1dbd69b0c0a2a81e706cd9
聪聪不是冲冲 <862348941@qq.com>
2025-05-21 13:49:43 +0800

    feat(index): restore home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,7 @@
 <script setup lang="ts">
-/**
- * index代替app组件
- */
 
-const appConfig = useAppConfig();
 </script>
 
 <template>
-  <h1>{{ appConfig.title }}</h1>
-  <h2>{{ appConfig.title }}</h2>
-  <h3>{{ appConfig.title }}</h3>
-  <div class="text-banner">{{ appConfig.title }}</div>
-  <div>{{ appConfig.title }}</div>
-  <div class="text-notes">{{ appConfig.title }}</div>
-
-  <button>了解更多</button>
-  <button class="button-sm">了解更多</button>
+  <NuxtWelcome></NuxtWelcome>
 </template>
